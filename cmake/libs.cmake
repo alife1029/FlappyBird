11167c0f9dd744e0ad14c0c670d9882d91128d08
Ali Efe GÜR <alife1029@gmail.com>
2023-09-29 00:23:52 +0300

    Window Creation

---
+++ cmake/libs.cmake
@@ -0,0 +1,6 @@
+add_subdirectory(libs/glfw)
+
+set(INCLUDE_DIRS	libs/glfw/include)
+
+set(LINK_LIBS		glfw
+					opengl32)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main(int argc, char** argv) {
-	std::cout << "Hello World" << std::endl;
+int main(int argc, char** argv) 
+{
+	if (!glfwInit()) 
+	{
+		std::cout << "Failed to initialize GLFW!" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Flappy Bird", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
